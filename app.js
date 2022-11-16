df122d1c03a9cf4d6374137d6041a7fad2951dcc
Karan Chauhan <karandemo577@gmail.com>
2022-11-16 22:17:16 +0530

    html and JS completed

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("script connected")
+let counterInp = document.getElementById("conterField")
+let output = document.getElementById("output")
+let result = document.createTextNode("");
+
+
+let counter = () => {
+    if (counterInp.value.trim() === "") {
+        console.log("please enter the word")
+        result.data = "0"
+    }else {
+        result.data = ""
+        let arrInp = counterInp.value.trim().split(" ");
+        result.data = arrInp.length
+    }
+    output.appendChild(result)
+}
